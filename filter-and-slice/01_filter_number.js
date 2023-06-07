2efb4bc264de7c90db051326642384c2a9766957
Alimnfl <mnaufallim234@gmail.com>
2023-06-08 03:23:41 +0700

    commit First 01_filter_number.js

---
+++ filter-and-slice/01_filter_number.js
@@ -0,0 +1,33 @@
+const numbers = [-50, -30, -10, 10, 40, 50];
+
+/*
+ * Filter have 3 arguments
+ * v : value, i : index, a : original array
+ */
+
+// Filter positive value
+const positive = numbers.filter((v) => v > 0);
+console.log(positive); // this value is [10, 40, 50]
+
+// .filter() returns an array, so we can map, filter or reduce it again
+const positiveUnder50 = numbers.filter((v) => v > 0).filter((v) => v < 50);
+console.log(positiveUnder50);
+
+// Add 10 to each positive value under 50 in positiveUnder50
+const addTen = positiveUnder50.map((v) => v + 10);
+console.log(addTen);
+
+// Filter Negative Value
+const negative = numbers.filter((v) => v < 0);
+console.log(negative);
+
+// Change Negative to Positive
+const negativeToPositive = negative.map((v) => ~v + 1);
+console.log(negativeToPositive);
+
+// Filter a number that have the next number + 1
+// 9 + 1 = 10 true, 11 + 1 = 12 false (the array doesn't have number 12)
+const num = [9, 10, 11, 13, 15, 17, 18];
+
+const hasNext = num.filter((v, _, a) => a.includes(v + 1));
+console.log(hasNext);
